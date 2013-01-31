7cce2ba1175450f0a8ee1a23c1805acaf2d13ee9
jba <jul.baudry@gmail.com>
2013-01-31 22:26:15 +0100

    add bootstrap example

--- mdl4ui-webapp/src/main/java/org/mdl4ui/sample/gwt/client/Sample.java
+++ mdl4ui-webapp/src/main/java/org/mdl4ui/sample/gwt/client/Sample.java
@@ -1,13 +1,64 @@
 package org.mdl4ui.sample.gwt.client;
 
+import org.mdl4ui.sample.gwt.client.ui.FieldView;
+
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.Form;
+import com.github.gwtbootstrap.client.ui.FormActions;
+import com.github.gwtbootstrap.client.ui.Legend;
+import com.github.gwtbootstrap.client.ui.PasswordTextBox;
+import com.github.gwtbootstrap.client.ui.base.TextBox;
+import com.github.gwtbootstrap.client.ui.constants.ButtonType;
+import com.github.gwtbootstrap.client.ui.constants.FormType;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Sample implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        RootPanel.get("content").add(new Label("It works!"));
+        Form form = new Form();
+        form.setType(FormType.HORIZONTAL);
+
+        Legend legend = new Legend("Form Validation with Control Decoration");
+        form.add(legend);
+
+        final TextBox userName = new TextBox();
+        userName.getElement().setAttribute("placeholder", "Username");
+        final FieldView userNameField = new FieldView(userName, "Username");
+        form.add(userNameField.asWidget());
+
+        final PasswordTextBox password = new PasswordTextBox();
+        password.getElement().setAttribute("placeholder", "Password");
+        final FieldView passwordField = new FieldView(password, "Password");
+        form.add(passwordField.asWidget());
+
+        FormActions actions = new FormActions();
+        Button validate = new Button("Validate Login");
+        validate.setType(ButtonType.PRIMARY);
+        actions.add(validate);
+        form.add(actions);
+
+        validate.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                String text = userName.getText();
+                if (text == null || "".equals(text.trim())) {
+                    userNameField.error("Please enter your username");
+                } else {
+                    userNameField.valid();
+                }
+                text = password.getText();
+                if (text == null || "".equals(text.trim())) {
+                    passwordField.error("Please enter your password");
+                } else {
+                    passwordField.valid();
+                }
+            }
+        });
+
+        RootPanel.get("content").add(form);
     }
 }
---
+++ mdl4ui-webapp/src/main/java/org/mdl4ui/sample/gwt/client/ui/FieldView.java
@@ -0,0 +1,43 @@
+package org.mdl4ui.sample.gwt.client.ui;
+
+import com.github.gwtbootstrap.client.ui.ControlGroup;
+import com.github.gwtbootstrap.client.ui.ControlLabel;
+import com.github.gwtbootstrap.client.ui.Controls;
+import com.github.gwtbootstrap.client.ui.HelpInline;
+import com.github.gwtbootstrap.client.ui.constants.ControlGroupType;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+public class FieldView implements IsWidget {
+
+    private final ControlGroup controlGroup;
+    private final HelpInline help;
+
+    public FieldView(IsWidget widget, String label) {
+        controlGroup = new ControlGroup();
+
+        ControlLabel controLabel = new ControlLabel(label);
+        controlGroup.add(controLabel);
+
+        Controls controls = new Controls();
+        help = new HelpInline();
+        controls.add(widget);
+        controls.add(help);
+        controlGroup.add(controls);
+    }
+
+    @Override
+    public Widget asWidget() {
+        return controlGroup;
+    }
+
+    public void error(String message) {
+        controlGroup.setType(ControlGroupType.ERROR);
+        help.setText(message);
+    }
+
+    public void valid() {
+        controlGroup.setType(ControlGroupType.SUCCESS);
+        help.setText(null);
+    }
+}
