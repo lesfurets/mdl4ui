5060b8e344f78aacc035e1ea0defdc7abf3f76b7
jba <jul.baudry@gmail.com>
2013-02-04 19:59:38 +0100

    remove WizardImpl

--- mdl4ui-webapp/src/main/java/org/mdl4ui/gwt/sample/client/Sample.java
+++ mdl4ui-webapp/src/main/java/org/mdl4ui/gwt/sample/client/Sample.java
@@ -1,5 +1,9 @@
 package org.mdl4ui.gwt.sample.client;
 
+import static org.mdl4ui.gwt.sample.client.ui.factory.GwtFieldHelpFactory.HELP;
+import static org.mdl4ui.gwt.sample.client.ui.factory.GwtFieldLabelFactory.LABEL;
+import static org.mdl4ui.gwt.sample.client.ui.factory.GwtFieldRendererFactory.RENDERER;
+
 import org.mdl4ui.fields.model.Screen;
 import org.mdl4ui.fields.model.Wizard;
 import org.mdl4ui.gwt.sample.client.ui.model.ScreenView;
@@ -7,16 +11,15 @@ import org.mdl4ui.ui.sample.EScreenSample;
 
 import com.github.gwtbootstrap.client.ui.FluidContainer;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Sample implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        Wizard wizard = GWT.create(Wizard.class);
-        final Screen screen = wizard.getScreen(EScreenSample.SCR_WIDGETS_SHOWCASE);
-        final ScreenView view = new ScreenView(screen);
+        Wizard wizard = new Wizard(LABEL, RENDERER, HELP);
+        Screen screen = wizard.getScreen(EScreenSample.SCR_WIDGETS_SHOWCASE);
+        ScreenView view = new ScreenView(screen);
 
         FluidContainer container = new FluidContainer();
         container.add(view);
