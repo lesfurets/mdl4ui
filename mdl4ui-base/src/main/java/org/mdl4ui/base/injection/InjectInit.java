e440558f5fd807cd596fb7df1c7d2636fdbf82af
gdigugli <gdigugli@gmail.com>
2013-02-19 17:48:12 +0100

    fixed Target for annotations

--- mdl4ui-base/src/main/java/org/mdl4ui/base/injection/InjectInit.java
+++ mdl4ui-base/src/main/java/org/mdl4ui/base/injection/InjectInit.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.TYPE)
+@Target(ElementType.ANNOTATION_TYPE)
 public @interface InjectInit {
 }
